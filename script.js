60aff124390cbda64ae296ebbc4bfed606ecdd00
JasonM0712 <155554008+JasonM0712@users.noreply.github.com>
2025-11-26 19:50:36 +0100

    Push 26/11/25 Modal sans css

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
+function openModal(){
+    document.querySelector('.background-modal').style.display = "block";
+    document.querySelector('.modal').classList.add('modal-open')
+}
 
+function closeModal(){
+    document.querySelector('.background-modal').style.display = "none";
+    document.querySelector('.modal').classList.remove('modal-open')
 
-// const closeModalButton = document.querySelector('bloc-article-button');
-//     button.addEventListener('click', function(){
-
-//     }
-// );
-
+}
 
     
             
