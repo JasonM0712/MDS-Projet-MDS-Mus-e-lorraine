db3a567115247f4a8c19a327dc52784d7389e12c
JasonM0712 <155554008+JasonM0712@users.noreply.github.com>
2025-12-03 16:04:42 +0100

    Regex Email 03/12/2025

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 function openModal(){
     document.querySelector('.background-modal').style.display = "block";
     document.querySelector('.modal').classList.add('modal-open')
@@ -8,6 +10,52 @@ function closeModal(){
     document.querySelector('.modal').classList.remove('modal-open')
 
 }
+
+let form = document.querySelector('#contactForm');
+
+form.email.addEventListener('change', function(){
+    emailValid(this)
+});
+
+function emailValid(inputEmail){
+        let emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-z]{2,3}$/;
+        let testEmail = emailRegex.test(inputEmail.value);
+        let small = inputEmail.nextElementSibling;
+// ^ nextElementSibling sert à attraper la balise qui est juste après
+
+
+            if(testEmail){
+                small.innerHTML = 'Adresse Valide';
+                small.classList.remove('emailFailed')
+                small.classList.add('emailSuccess')
+            } 
+            else{
+                small.innerHTML = 'Adresse non valide';
+                small.classList.remove('emailSuccess')
+                small.classList.add('emailFailed')
+            };
+
+
+};
+
+
+
+
+
+
+// if(email == regex){
+//     document.addEventListener(validation)
+// }
+
+// ____________________________________________________________________________________________________________________________
+// alert('Je suis JavaScript')
+
+// Alerte avec variable
+
+// let message; 
+// message ='Jason'
+// alert(message);
+
 
     
             
